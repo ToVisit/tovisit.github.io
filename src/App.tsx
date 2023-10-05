6ce59e58c6c1c3a75090ab9f70c3dbda1bb38fdb
Damian Perera <damianperera@outlook.com>
2023-10-05 15:41:24 +0200

    removed name

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="https://images.tovisit.eu/tovisit/logo/png/logo-no-background.png" className="App-logo" alt="logo" />
-        <h3>To Visit EU</h3>
       </header>
     </div>
   );
