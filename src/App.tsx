10a44c0fd3c399270d6bca0349c9da3633d4c630
Damian Perera <damianperera@outlook.com>
2024-11-24 10:27:06 +0100

    added restaurants

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <header>
-        <img src="https://tovisit.github.io/images//tovisit/logo/png/logo-no-background.png" alt="Logo" />
+        <img src="https://tovisit.github.io/images/tovisit/logo/png/logo-no-background.png" alt="Logo" />
         <div className="logo-text">
           <h1>Europe</h1>
           <p>Travel · Tours · Tips</p>
@@ -13,45 +13,45 @@ function App() {
       </header>
       <div className="container">
         <div className="grid">
-          <div className="card selectable" onClick={() => { window.open('http://museums.tovisit.eu', '_blank') }}>
-            <img src="https://tovisit.github.io/images//tovisit/cards/museums-low.jpeg" alt="Museums" />
+          <div className="card selectable" onClick={() => { window.open('https://maps.app.goo.gl/33MfBF8N7miEF7s17', '_blank') }}>
+            <img src="https://tovisit.github.io/images/tovisit/cards/museums-low.jpeg" alt="Museums" />
             <div className="card-content">
               <h2>Museums</h2>
-              <h6>museums.tovisit.eu</h6>
+              {/* <h6>museums.tovisit.eu</h6> */}
               <p>Explore Europe's Finest Museums & Galleries: A Curated Guide</p>
             </div>
           </div>
-          <div className="card selectable" onClick={() => { window.open('http://bars.tovisit.eu', '_blank') }}>
-            <img src="https://tovisit.github.io/images//tovisit/cards/bars-low.jpeg" alt="Bars & Pubs" />
+          <div className="card selectable" onClick={() => { window.open('https://maps.app.goo.gl/8hD7Bncaxw2CeN4y7', '_blank') }}>
+            <img src="https://tovisit.github.io/images/tovisit/cards/bars-low.jpeg" alt="Bars & Pubs" />
             <div className="card-content">
               <h2>Bars</h2>
-              <h6>bars.tovisit.eu</h6>
+              {/* <h6>bars.tovisit.eu</h6> */}
               <p>Explore Europe's Best: Our Curated List of Top Bars & Pubs</p>
             </div>
           </div>
-          <div className="card coming-soon">
-            <img src="https://tovisit.github.io/images//tovisit/cards/restaurants-low.jpeg" alt="Restaurants" />
+          <div className="card selectable" onClick={() => { window.open('https://maps.app.goo.gl/RVCvuXx59VreMCr3A', '_blank') }}>
+            <img src="https://tovisit.github.io/images/tovisit/cards/restaurants-low.jpeg" alt="Restaurants" />
             <div className="card-content">
               <h2>Restaurants</h2>
-              <h6>food.tovisit.eu</h6>
-              <p>Coming Soon</p>
+              {/* <h6>food.tovisit.eu</h6> */}
+              <p>Explore curated restaurants - from hole-in-the-wall spots to cozy diners</p>
               <br/>
             </div>
           </div>
           <div className="card coming-soon">
-            <img src="https://tovisit.github.io/images//tovisit/cards/cafes-low.jpeg" alt="Cafés" />
+            <img src="https://tovisit.github.io/images/tovisit/cards/cafes-low.jpeg" alt="Cafés" />
             <div className="card-content">
               <h2>Cafés</h2>
-              <h6>cafes.tovisit.eu</h6>
+              {/* <h6>cafes.tovisit.eu</h6> */}
               <p>Coming Soon</p>
               <br/>
             </div>
           </div>
           <div className="card coming-soon">
-            <img src="https://tovisit.github.io/images//tovisit/cards/lakes-low.jpeg" alt="Lakes" />
+            <img src="https://tovisit.github.io/images/tovisit/cards/lakes-low.jpeg" alt="Lakes" />
             <div className="card-content">
               <h2>Lakes</h2>
-              <h6>lakes.tovisit.eu</h6>
+              {/* <h6>lakes.tovisit.eu</h6> */}
               <p>Coming Soon</p>
               <br/>
             </div>
